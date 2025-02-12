36487c4c67dd2b492d7b9c108d71485ace4bffce
Lior Shilon <shilon199707@gmail.com>
2025-02-12 02:25:13 +0200

    (ADD) Added method get_image_paths: returns a list of all the image file paths under the specified experiment

--- dataset_loader/experiment_loader.py
+++ dataset_loader/experiment_loader.py
@@ -8,6 +8,7 @@ class ExperimentLoader:
 
     def __init__(self, experiment_path):
         self.experiment_path = experiment_path
+        self.image_formats = ('.png', '.jpg', '.jpeg', '.bmp', '.tiff')
 
     def image_generator(self):
         """Generator that yields images and their filenames."""
@@ -15,7 +16,7 @@ class ExperimentLoader:
         def generator():
             for file_name in os.listdir(self.experiment_path):
                 file_path = os.path.join(self.experiment_path, file_name)
-                if file_path.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
+                if self._is_image(file_path):
                     # TODO: Switch between grayscale and RGB
                     # mage = cv2.imread(file_path)
                     image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
@@ -23,3 +24,9 @@ class ExperimentLoader:
                         yield image, file_name
 
         return generator  # Returns generator function
+
+    def get_image_paths(self):
+        return [os.path.join(self.experiment_path, file_name) for file_name in os.listdir(self.experiment_path) if self._is_image(file_name)]
+
+    def _is_image(self, file_path):
+        return file_path.lower().endswith(self.image_formats)
