769391a7558e184316b1d88382a3c84f50143a87
Lior Shilon <shilon199707@gmail.com>
2025-02-13 02:16:47 +0200

    (ADD) Added Class UltrasoundDataset. responsible for loading the image paths of a specific experiment or all the experiments under a specific lable

--- dataset_loader/__init__.py
+++ dataset_loader/__init__.py
@@ -1,4 +1,5 @@
 from .experiment_loader import ExperimentLoader
 from .image_database import ImageDatabase
+from .ultrasound_dataset import UltrasoundDataset
 
-__all__ = ["ExperimentLoader", "ImageDatabase"]
+__all__ = ["ExperimentLoader", "ImageDatabase", "UltrasoundDataset"]
---
+++ dataset_loader/ultrasound_dataset.py
@@ -0,0 +1,42 @@
+import torch
+from torchgen.executorch.api.et_cpp import returntype_type
+from torchvision import transforms
+from PIL import Image
+from dataset_loader.image_database import ImageDatabase
+
+
+
+# Dataset Loader
+class UltrasoundDataset(torch.utils.data.Dataset):
+    # Image transformations (grayscale, resize, normalization)
+    TRANSFORM = transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),  # Ensure grayscale
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5], std=[0.5])  # Normalize to [-1,1]
+    ])
+    def __init__(self, root_dir,config_file,label,experiment_name=None, transform=None):
+        self.root_dir = root_dir
+        self.config_file = config_file
+        self.label = label
+        self.experiment_name = experiment_name
+        self.transform = UltrasoundDataset.TRANSFORM if not transform else transform
+        self.image_paths = self._load_images_paths()
+
+    def __len__(self):
+        return len(self.get_image_paths())
+
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        image = Image.open(img_path).convert("L")  # Convert to grayscale
+        if self.transform:
+            image = self.transform(image)
+        return image
+
+    def _load_images_paths(self):
+        db = ImageDatabase(self.root_dir, self.config_file)
+        if not self.experiment_name:
+            return db.all_images_paths(self.label)
+        return db.get_experiment_image_paths(self.label, self.experiment_name)
+
+    def get_image_paths(self):
+        return self.image_paths
